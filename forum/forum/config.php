cf3e296b3e6ff15be6c04f0a15385470d8fdf1a3
Felix Niklas <mrflix@gmail.com>
2014-12-16 13:32:41 +0100

    remove old test config

--- forum/forum/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-c::set('forum.token', 'dahoam is dahoam');
-c::set('forum.twitter.key', 'CitI5BADEWl6bw2DuOCEkoFy8');
-c::set('forum.twitter.secret', 'dMhZeqMP7MDdk4Zk7qGNbhXMjbuHVP81au9c1VZKylqjUTMbvw');
-c::set('forum.users.banned', array());
-c::set('forum.users.admins', array('mrflix'));
-
-c::set('timezone', 'Europe/Berlin');

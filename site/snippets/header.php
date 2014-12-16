2fa8f86644f155adf7f3d7ecf69e684a6316a88b
Felix Niklas <mrflix@gmail.com>
2014-12-16 12:10:00 +0100

    add favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,7 +3,7 @@
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
-<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<link rel="shortcut icon" href="<?= url('assets/images/favicon.ico') ?>">
 
 <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
 <meta name="description" content="<?= $site->description()->html() ?>">

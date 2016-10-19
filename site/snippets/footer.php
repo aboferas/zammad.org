158c7122da62a849e5bac348cef50bb91e855aaa
Martin Edenhofer <me@edenhofer.de>
2016-10-19 09:21:37 +0200

    Show links.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -10,3 +10,16 @@
 <?= js('assets/js/prism.js') ?>
 <?= js('assets/js/autosize.min.js') ?>
 <?= js('assets/js/jquery.caret.min.js') ?>
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik.znuny.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '9']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//piwik.znuny.com/piwik.php?idsite=9" style="border:0;" alt="" /></p></noscript>

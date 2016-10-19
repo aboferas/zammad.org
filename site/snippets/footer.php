37188569f40d510b329937f25efb596c0850db7f
Martin Edenhofer <me@edenhofer.de>
2016-10-19 04:50:14 +0200

    Fixed jquery url.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -6,7 +6,7 @@
 
 </footer>
 
-<?= js('assets/js/jquery.js') ?>
+<?= js('assets/js/jquery-2.1.1.min.js') ?>
 <?= js('assets/js/prism.js') ?>
 <?= js('assets/js/autosize.min.js') ?>
 <?= js('assets/js/jquery.caret.min.js') ?>

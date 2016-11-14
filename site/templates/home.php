518b425e95a7d7318f4bdc0dafedeb3c79de65f2
Martin Edenhofer <me@edenhofer.de>
2016-11-14 10:16:38 +0100

    Added 1.1 release note.

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,6 +3,9 @@
 <div class="hero" style="background-image: url(<?= $page->images()->find('background.jpg')->url() ?>)">
   <div class="wrapper">
     <div class="header">
+
+      <a class="subtitle" href="https://zammad.com/news" style="color: hsl(146,51%,50%); text-decoration: underline;">Note: Zammad 1.1 has been released (read more)</a>
+
       <h1><?= $page->header() ?></h1>
       <div class="subtitle"><?= $page->subtitle()->kirbytext() ?></div>
     </div>
